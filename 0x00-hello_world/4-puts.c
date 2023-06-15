8c7974ccad5e0dab6454735abcb9cb19b984b127
Mohamed-habed <habedmed@gmail.com>
2023-06-15 15:39:24 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 /**
- * main - Entry point
- *
- * Return: return(0) (Success)
+ * main - A program that prints a line with puts function
+ * Retur: 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}|	
+}
