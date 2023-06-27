96ff186944e49017a563a75220a7d05357c50fb5
Mohamed-habed <habedmed@gmail.com>
2023-06-26 18:04:54 -0700

    hihe

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*_strlen - Returns the  length of a string.
+*@s: String to be measured.
+*
+*Return: Length of the string.
+*/
+int strlen(char *s)
+{
+int length = 0;
+while (*s != '10')
+{ length++;
+S++;
+return (length);
+}
