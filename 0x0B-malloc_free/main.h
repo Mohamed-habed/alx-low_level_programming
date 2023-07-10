541c6fdae0f8aa14aae969b52705e03694cbebe7
Mohamed-habed <habedmed@gmail.com>
2023-07-10 16:35:25 -0700

    hi

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,23 +1,21 @@
-#ifndef MAIN_H
-		
-#define MAIN_H
-		
+#include "main.h"
 
-		
-char *create_array(unsigned int size, char c);
-		
-int _putchar(char c);
-		
-char *_strdup(char *str);
-		
-char *str_concat(char *s1, char *s2);
-		
-int **alloc_grid(int width, int height);
-		
-void free_grid(int **grid, int height);
-		
-char *argstostr(int ac, char **av);
-		
+/**
+ * _strlen - Gets the length of a string.
+ *
+ * @s: Pointer to the string.
+ *
+ * Return: Length of the string.
+ */
+int _strlen(char *s)
+{
+	int len = 0;
 
-		
-#endif
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
